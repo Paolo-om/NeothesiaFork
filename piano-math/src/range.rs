700679e1ce518746b767da6b3bd28124a7542a1c
itsyshaus <e2theugen@gmail.com>
2023-05-07 23:31:00 +0200

    Update range.rs

--- piano-math/src/range.rs
+++ piano-math/src/range.rs
@@ -1,24 +1,25 @@
-#![allow(dead_code)]
+#![allow(dead_code)] // allows unused code to prevent warnings
 
-use std::ops::{Range, RangeBounds};
-
+// Define some constants for black keys
 const KEY_CIS: u8 = 1;
 const KEY_DIS: u8 = 3;
 const KEY_FIS: u8 = 6;
 const KEY_GIS: u8 = 8;
 const KEY_AIS: u8 = 10;
 
+// Define a struct for a piano key ID
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub struct KeyId(u8);
 
 impl KeyId {
+    // Determine whether the key is black or white
     pub fn is_black(&self) -> bool {
         let key = self.0 % 12;
         key == KEY_CIS || key == KEY_DIS || key == KEY_FIS || key == KEY_GIS || key == KEY_AIS
     }
 }
 
-/// Describe used slice of piano keyboard
+// Define a struct for a range of piano keys
 #[derive(Debug, Clone)]
 pub struct KeyboardRange {
     range: Range<u8>,
@@ -29,6 +30,7 @@ pub struct KeyboardRange {
 }
 
 impl KeyboardRange {
+    // Create a new keyboard range
     pub fn new<R>(range: R) -> Self
     where
         R: RangeBounds<usize>,
@@ -37,6 +39,7 @@ impl KeyboardRange {
         let mut white_keys = Vec::new();
         let mut black_keys = Vec::new();
 
+        // Convert the range bounds to u8 values
         let start = range.start_bound();
         let end = range.end_bound();
 
@@ -54,6 +57,7 @@ impl KeyboardRange {
 
         let range = start..end;
 
+        // Create the KeyId vectors for the range
         for id in range.clone().map(KeyId) {
             keys.push(id);
 
@@ -73,32 +77,39 @@ impl KeyboardRange {
         }
     }
 
+    // Create a standard range for 88 keys
     pub fn standard_88_keys() -> Self {
         Self::new(21..=108)
     }
 }
 
 impl KeyboardRange {
+    // Check whether an item is within the range
     pub fn contains(&self, item: u8) -> bool {
         self.range.contains(&item)
     }
 
+    // Get the total count of keys in the range
     pub fn count(&self) -> usize {
         self.keys.len()
     }
 
+    // Get the count of white keys in the range
     pub fn white_count(&self) -> usize {
         self.white_keys.len()
     }
 
+    // Get the count of black keys in the range
     pub fn black_count(&self) -> usize {
         self.black_keys.len()
     }
 
+    // Get an iterator for all keys in the range
     pub fn iter(&self) -> std::slice::Iter<KeyId> {
         self.keys.iter()
     }
 
+    // Get an iterator for white keys in the range
     pub fn white_iter(&self) -> std::slice::Iter<KeyId> {
         self.white_keys.iter()
     }
